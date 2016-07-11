5a9071dd71c7c1f196948c6037c75d8122f6f4ef
Pat Hadley <pat@pathadley.net>
2016-07-11 19:34:18 +0100

    basic user inbound routes

---
+++ tourista/front-end/js/controllers/usersController.js
@@ -0,0 +1,62 @@
+angular
+  .module('Tourista')
+  .controller('UsersController', UsersController);
+
+UsersController.$inject = ['User', 'CurrentUser'];
+function UsersController(User, CurrentUser){
+
+  var self = this;
+
+  self.all           = [];
+  self.user          = null;
+  self.currentUser   = null;
+  self.error         = null;
+  self.getUsers      = getUsers;
+  self.register      = register;
+  self.login         = login;
+  self.logout        = logout;
+  self.checkLoggedIn = checkLoggedIn;
+
+  function getUsers() {
+    User.query(function(data){
+      self.all = data.users;
+    });
+  }
+
+  function handleLogin(res) {
+    console.log(res);
+
+    var token = res.token ? res.token : null
+    if (token) {
+      // save the token in local storage
+       // save the current user with the token
+    }
+  }
+
+  function handleError(e) {
+    self.error = "Something went wrong.";
+  }
+
+  function register() {
+    User.register(self.user, handleLogin, handleError);
+  }
+
+  function login() {
+    User.login(self.user, handleLogin, handleError);
+  }
+
+  function logout() {
+  }
+
+  function checkLoggedIn() {
+    self.currentUser = CurrentUser.getUser();
+    return !!self.currentUser;
+  }
+
+  if (checkLoggedIn()) {
+    self.getUsers();
+  }
+
+  return self;
+
+}
