ed7e74d3faab156cb3d5bba6e19a4d172059a339
Pat Hadley <pat@pathadley.net>
2016-07-12 16:10:22 +0100

    slow filepicker progress

--- tourista/front-end/js/app.js
+++ tourista/front-end/js/app.js
@@ -8,30 +8,59 @@ angular
     'ngMessages'
     ])
   .constant('API', 'http://localhost:3000/api')
-  .directive('fpCustomDirective', function(filepickerService){
-      return {
-          scope: {
-              options: {mimetype: "image/*"},
-              onSuccess:'&',
-              onError:'&',
-          },
-          link: function(scope, elm, attrs) {
-              scope.openPicker = openPicker;
-              scope.options = scope.options || {};
-              function openPicker(){
-                  filepickerService.pick(
-                      scope.options,
-                      function(Blob){
-                          scope.onSuccess({Blob: Blob});
-                      },
-                      function(Error){
-                          scope.onError({Error: Error});
-                      }
-                  );
-              }
-          }
-      };
+  .controller('GalleryCtrl', function ($scope, filepickerService, $window) {
+    $scope.files = JSON.parse($window.localStorage.getItem('files') || '[]');
+
+    $scope.options = {
+      mimetype: 'image/*',
+      services: [
+        'BOX',
+        'COMPUTER',
+        'DROPBOX',
+        'FACEBOOK',
+        'GMAIL',
+        'IMAGE_SEARCH',
+        'FLICKR',
+        'GOOGLE_DRIVE',
+        'SKYDRIVE',
+        'PICASA',
+        'URL',
+        'INSTAGRAM'
+      ]};
+    $scope.onSuccess = onSuccess;
+
+    function onSuccess(Blob){
+      $scope.files.push(Blob);
+      $window.localStorage.setItem('files', JSON.stringify($scope.files));
+      $scope.$apply();
+    }
   })
+  .directive('fpCustomDirective', function(filepickerService){
+    return {
+      scope: {
+        options: '=',
+        onSuccess:'&',
+        onError:'&',
+      },
+      template: '<button class="fp__btn" ng-click="openPicker()">Pick</button>',
+      link: function(scope, elm, attrs) {
+        scope.openPicker = openPicker;
+        // scope.options = {};
+        function openPicker(){
+          filepickerService.pick(
+            scope.options,
+            function(Blob){
+                scope.onSuccess({Blob: Blob});
+                console.log("blob", Blob);
+            },
+            function(Error){
+                scope.onError({Error: Error});
+            }
+          );
+        }
+      }
+    };
+    })
   .config(MainRouter)
   .config(function (filepickerProvider) {
     filepickerProvider.setKey('Aaf507RcMRMid6D1ej8D1z');
@@ -47,7 +76,8 @@ function MainRouter($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: "/",
-      templateUrl: "./js/views/home.html"
+      templateUrl: "./js/views/home.html",
+      controller: 'GalleryCtrl'
     })
     .state('login', {
       url: "/login",
@@ -73,3 +103,4 @@ function MainRouter($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise("/");
 };
+
